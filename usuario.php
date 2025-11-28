ff98daa854221955ec6b589d500e58cdb852ed01
viniciolimadev <dev.vinicio@gmail.com>
2025-11-28 16:19:39 -0300

    manipulando dados de usuario

--- usuario.php
+++ usuario.php
@@ -2,5 +2,23 @@
 
 $email = 'dev.vinicio@gmail.com';
 
-echo substr($email, 0, 11) . PHP_EOL;
-echo substr($email, 12) . PHP_EOL;
+$posicaoDoArroba = strpos($email, '@');
+
+$senha = '123456dadadw';
+
+echo mb_strlen($senha) . PHP_EOL;
+
+if(strlen($senha) >= 8) {
+    echo "Senha segura" . PHP_EOL;
+} else {
+    echo "Senha insegura" . PHP_EOL;
+} 
+
+$usuario = substr($email, 0, $posicaoDoArroba);
+
+echo mb_strtoupper($usuario) . PHP_EOL;
+
+echo strtolower($usuario) . PHP_EOL;
+
+echo $usuario . PHP_EOL;
+echo substr($email, $posicaoDoArroba+1) . PHP_EOL;
